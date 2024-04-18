0b83c0a1c396a0ad0cca996b091f03a272d0a173
p2 <mongaifushutsu@yahoo.co.jp>
2024-04-18 14:03:27 +0900

    カリキュラム50の途中です

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App¥Http¥Controllers;
+use Illuminate¥Http¥Reqeuest;
+
+class TestController extends Controller
+{
+    public function func() {
+        return view('sample');
+    }
+}
+
