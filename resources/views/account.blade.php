6b6e40527e4f607742478f03c35fc2310f689c42
p2 <mongaifushutsu@yahoo.co.jp>
2024-12-13 12:38:47 +0900

    2024年12月13日

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -14,6 +14,11 @@
                             <th>編集</th>
                             <th>削除</th>
                             <th>名前</th>
+                            <th>メールアドレス</th>
+                            <th>電話番号</th>
+                            <th>都道府県</th>
+                            <th>市町村</th>
+                            <th>番地・アパート名</th>
                         </tr>
                     </thead>
                     <!-- {{-- テーブルのボディ部分はこの中に記述 --}}
@@ -25,10 +30,106 @@
                         <tr>
                             <!-- {{-- <td> => テーブルデータ => 1セル分のデータを表す --}} -->
                             <td>{{ $user->id }}</td>
+                            <td>{{ $user->id }}</td>
                             <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->email }}</td>
                             <td>{{ $user->email }}</td>
                         </tr>
                     @endforeach
+                    </tbody>
+                </table>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>編集</th>
+                            <th>削除</th>
+                            <th>名前</th>
+                            <th>メールアドレス</th>
+                            <th>電話番号</th>
+                            <th>都道府県</th>
+                            <th>市町村</th>
+                            <th>番地・アパート名</th>
+                        </tr>
+                        <tr>
+                            <!-- <th>削除</th> -->
+                            <td>edit</td>
+                            <td>delete</td>
+                            <td>name</td>
+                            <td>email</td>
+                            <td>telephone</td>
+                            <td>prefectures</td>
+                            <td>Municipality</td>
+                            <td>Street address/apartment name</td>
+                        </tr>
+                        <tr>
+                            <!-- <th>名前</th> -->
+                            <td>edit</td>
+                            <td>delete</td>
+                            <td>name</td>
+                            <td>email</td>
+                            <td>telephone</td>
+                            <td>prefectures</td>
+                            <td>Municipality</td>
+                            <td>Street address/apartment name</td>
+                        </tr>
+                        <tr>
+                            <!-- <th>メールアドレス</th> -->
+                            <td>edit</td>
+                            <td>delete</td>
+                            <td>name</td>
+                            <td>email</td>
+                            <td>telephone</td>
+                            <td>prefectures</td>
+                            <td>Municipality</td>
+                            <td>Street address/apartment name</td>
+                        </tr>
+                        <tr>
+                            <!-- <th>電話番号</th> -->
+                            <td>edit</td>
+                            <td>delete</td>
+                            <td>name</td>
+                            <td>email</td>
+                            <td>telephone</td>
+                            <td>prefectures</td>
+                            <td>Municipality</td>
+                            <td>Street address/apartment name</td>
+                        </tr>
+                        <tr>
+                            <!-- <th>都道府県</th> -->
+                            <td>edit</td>
+                            <td>delete</td>
+                            <td>name</td>
+                            <td>email</td>
+                            <td>telephone</td>
+                            <td>prefectures</td>
+                            <td>Municipality</td>
+                            <td>Street address/apartment name</td>
+                        </tr>
+                        <tr>
+                            <!-- <th>市町村</th> -->
+                            <td>edit</td>
+                            <td>delete</td>
+                            <td>name</td>
+                            <td>email</td>
+                            <td>telephone</td>
+                            <td>prefectures</td>
+                            <td>Municipality</td>
+                            <td>Street address/apartment name</td>
+                        </tr>
+                        <tr>
+                            <!-- <th>番地・アパート名</th> -->
+                            <td>edit</td>
+                            <td>delete</td>
+                            <td>name</td>
+                            <td>email</td>
+                            <td>telephone</td>
+                            <td>prefectures</td>
+                            <td>Municipality</td>
+                            <td>Street address/apartment name</td>
+                        </tr>
                     </tbody>
                 </table>
                                 
