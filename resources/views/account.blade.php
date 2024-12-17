949932c4c08a9f753052cd59147af64f8acb6cae
p2 <mongaifushutsu@yahoo.co.jp>
2024-12-17 14:45:28 +0900

    2024年12月17日

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -33,7 +33,7 @@
                             <td>{{ $user->id }}</td>
                             <td>{{ $user->name }}</td>
                             <td>{{ $user->email }}</td>
-                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->postal_code }}</td>
                             <td>{{ $user->email }}</td>
                             <td>{{ $user->email }}</td>
                             <td>{{ $user->email }}</td>
